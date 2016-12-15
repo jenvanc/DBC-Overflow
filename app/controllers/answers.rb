89d3cc68969b04cbb0ede80463baf328c4d2ece1
Felix Vandelanotte & Patrick Kolodgy <pair+felixvdl.pkolodgy@devbootcamp.com>
2016-12-15 17:43:02 -0500

    Add buttons for editing and deleting answers, create partials for answers and comments

---
+++ app/controllers/answers.rb
@@ -0,0 +1,15 @@
+get '/questions/:question_id/answers/new' do
+  @question = Question.find(params[:question_id])
+  erb :'answers/new'
+end
+
+post '/questions/:question_id/answers' do
+  @question = Question.find(params[:question_id])
+  @answer = Answer.new(content: params[:content], user: current_user, question: @question)
+  if @answer.save
+    redirect "/questions/#{params[:question_id]}"
+  else
+    @errors = @answer.errors.full_messages
+    erb :'answers/new'
+  end
+end
