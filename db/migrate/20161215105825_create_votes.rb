bc03426eeb84079245b9fc987b1743ea5ca895ce
Felix Vandelanotte & Jenna Van Conett & Nadav Appel & Patrick Kolodgy <pair+felixvdl.jenvanc.nadavp3.pkolodgy@devbootcamp.com>
2016-12-15 11:15:20 -0500

    Add migrations and model files

---
+++ db/migrate/20161215105825_create_votes.rb
@@ -0,0 +1,10 @@
+class CreateVotes < ActiveRecord::Migration
+  def change
+    create_table :votes do |t|
+      t.belongs_to :user, null: false
+      t.references :voteable, polymorphic: true
+
+      t.timestamps(null: false)
+    end
+  end
+end
