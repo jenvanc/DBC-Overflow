83609af7c01fc6b358c0a8bc92ea55bcc7715d7b
Felix Vandelanotte & Patrick Kolodgy <pair+felixvdl.pkolodgy@devbootcamp.com>
2016-12-15 12:26:41 -0500

    Update model validations

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,5 +2,5 @@ class Vote < ActiveRecord::Base
   belongs_to :user
   validates :user, presence: true
   belongs_to :voteable, polymorphic: true
-  validates :user_id, scope: [:voteable_id, :voteable_type], unique: true
+  validates :user_id, scope: [:voteable_id, :voteable_type], uniqueness: true
 end
